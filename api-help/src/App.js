16f104fc3afa1007cab860bc9ab6f8d280500009
Paulino101 <89439038+Paulino101@users.noreply.github.com>
2022-04-05 17:01:27 -0700

    u

--- api-help/src/App.js
+++ api-help/src/App.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useState } from "react";
+import MapArray from "./components/MapArray";
 
 function App() {
   const url = "https://randomuser.me/api/?results=3";
@@ -11,10 +12,13 @@ function App() {
       console.log(array);
     });
   };
-  // i want to extract the first name from this api request but i keep getting errors when using {array.results.name.first}
+  // i want to extract the first name from this api request and pass it as a prop,  but i keep getting errors
   return(
     <div>
      <button onClick={apiGet}>make api request</button>
+     {array.map(arr => {(
+      <MapArray name={array.results.name.first}/>
+     )})}
     </div>
   );
 }
